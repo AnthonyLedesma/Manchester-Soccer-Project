484b5bcaa2e1824820ef051381cb42f0ca5e05c4
paranoia1906 <anthonymledesma@gmail.com>
2018-08-02 09:58:50 -0700

    Added footer module to project and integrated logo.

---
+++ src/Components/header_footer/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { CityLogo } from '../ui/icons';
+
+const Footer = () => {
+    return (
+        <footer className="bck_blue">
+        <div className="footer_logo">
+            <CityLogo
+                width="70px"
+                height="70px"
+                link={true}
+                linkTo="/"
+            />
+        </div>
+        <div className="footer_discl">
+            Anthony Ledesma - Manchester Soccer Project 2018. All Rights Reserved.
+        </div>
+            
+        </footer>
+    );
+};
+
+export default Footer;
--- src/Hoc/Layout.js
+++ src/Hoc/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Components/header_footer/Header';
+import Footer from '../Components/header_footer/Footer';
 
 const Layout = (props) => {
     return (
         <div>
             <Header/>
             {props.children}
+            <Footer/>
         </div>
     );
 };
