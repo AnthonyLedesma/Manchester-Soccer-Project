6077a848a02b55670ea1c7e34fa9e0e0d2be4ccb
paranoia1906 <anthonymledesma@gmail.com>
2018-08-02 14:14:40 -0700

    Finished Home Featured area including adding animation for featured player. Animations using react-move library.

--- src/Components/home/featured/Text.js
+++ src/Components/home/featured/Text.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { easePolyOut } from 'd3-ease';
 import Animate from 'react-move/Animate';
+import FeaturedPlayer from '../../../Resources/images/featured_player.png';
 
 class Text extends Component {
 
@@ -32,10 +33,102 @@ class Text extends Component {
         </Animate>
     )
 
+    animateFirst = () => (
+        <Animate
+            show={true}
+            start={{
+                opacity:0,
+                x:503,
+                y:450
+            }}
+            enter={{
+                opacity:[1],
+                x:[273],
+                y:[450],
+                timing:{duration: 1000, ease:easePolyOut}
+            }}
+        >
+            {({opacity, x,y})=>{
+                return(
+                    <div className="featured_first"
+                        style={{
+                            opacity,
+                            transform: `translate(${x}px,${y}px)`
+                        }}
+                    >
+                    League
+                    </div>
+                )
+            }}
+        </Animate>
+    )
+
+    animateSecond = () => (
+        <Animate
+            show={true}
+            start={{
+                opacity:0,
+                x:503,
+                y:586
+            }}
+            enter={{
+                opacity:[1],
+                x:[273],
+                y:[586],
+                timing:{delay:300, duration: 1000, ease:easePolyOut}
+            }}
+        >
+            {({opacity, x,y})=>{
+                return(
+                    <div className="featured_second"
+                        style={{
+                            opacity,
+                            transform: `translate(${x}px,${y}px)`
+                        }}
+                    >
+                    Championships
+                    </div>
+                )
+            }}
+        </Animate>
+    )
+
+    animatePlayer = () => (
+        <Animate
+            show={true}
+            start={{
+                opacity:0
+            }}
+            enter={{
+                opacity:[1],
+                x:[273],
+                y:[586],
+                timing:{delay:800, duration: 1000, ease:easePolyOut}
+            }}
+        >
+            {({opacity})=>{
+                return(
+                    <div className="featured_player"
+                        style={{
+                            opacity,
+                            background: `url(${FeaturedPlayer})`,
+                            transform: `translate(550px,201px)`
+                        }}
+                    >
+                    
+                    </div>
+                )
+            }}
+        </Animate>
+    )
+
     render() {
         return (
             <div className="featured_text">
+                {this.animatePlayer()}
                 {this.animateNumber()}
+                {this.animateFirst()}
+                {this.animateSecond()}
                 
             </div>
         );
