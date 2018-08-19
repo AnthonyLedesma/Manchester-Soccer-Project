505d8b74ae92d10e93e50946d477b9fa89a5a178
paranoia1906 <anthonymledesma@gmail.com>
2018-08-19 10:20:35 -0700

    Added delete player button to all edit pagess.

--- src/Components/admin/players/addEditPlayers.js
+++ src/Components/admin/players/addEditPlayers.js
@@ -16,9 +16,6 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-
-
-
 class AddEditPlayers extends Component {
 
     state = {
