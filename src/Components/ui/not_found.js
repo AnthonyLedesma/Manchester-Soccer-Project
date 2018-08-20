7ab226c69a8ecf69f6a40afbe7587bca9d20997f
paranoia1906 <anthonymledesma@gmail.com>
2018-08-20 07:41:47 -0700

    Adding title for the 404 page.

--- src/Components/ui/not_found.js
+++ src/Components/ui/not_found.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {Helmet} from "react-helmet";
+
 
 const NotFound = () => {
     return (
         <div className="not_found_container">
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>404 - Not Found</title>
+            </Helmet>
             <div>Sorry :(</div>
             <div>Not Found</div>
         </div>
